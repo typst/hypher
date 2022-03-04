d27e4bc9a65ee702c05605462198636e0e507898
Laurenz <laurmaedje@gmail.com>
2022-03-04 13:50:46 +0100

    Build patterns with build script

--- build.rs
+++ build.rs
@@ -1,23 +1,184 @@
 use std::collections::HashMap;
+use std::env;
+use std::fs::{self, File};
+use std::io::{self, BufWriter, Write};
+use std::path::{Path, PathBuf};
+
+fn main() -> io::Result<()> {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let languages = [
+        ("Afrikaans", "af", "Latn", "hyph-af.tex", 1, 2),
+        ("Belarusian", "be", "Cyrl", "hyph-be.tex", 2, 2),
+        ("Bulgarian", "bg", "Cyrl", "hyph-bg.tex", 2, 2),
+        ("Danish", "da", "Latn", "hyph-da.tex", 2, 2),
+        ("German", "de", "Latn", "hyph-de-1996.tex", 2, 2),
+        ("Greek", "el", "Grek", "hyph-el-monoton.tex", 1, 1),
+        ("English", "en", "Latn", "hyph-en-us.tex", 2, 3),
+        ("Spanish", "es", "Latn", "hyph-es.tex", 2, 2),
+        ("Estonian", "et", "Latn", "hyph-et.tex", 2, 3),
+        ("Finnish", "fi", "Latn", "hyph-fi.tex", 2, 2),
+        ("French", "fr", "Latn", "hyph-fr.tex", 2, 2),
+        ("Croatian", "hr", "Latn", "hyph-hr.tex", 2, 2),
+        ("Hungarian", "hu", "Latn", "hyph-hu.tex", 2, 2),
+        ("Icelandic", "is", "Latn", "hyph-is.tex", 2, 2),
+        ("Italian", "it", "Latn", "hyph-it.tex", 2, 2),
+        ("Georgian", "ka", "Geor", "hyph-ka.tex", 1, 2),
+        ("Kurmanji", "ku", "Latn", "hyph-kmr.tex", 2, 2),
+        ("Latin", "la", "Latn", "hyph-la.tex", 2, 2),
+        ("Lithuanian", "lt", "Latn", "hyph-lt.tex", 2, 2),
+        ("Mongolian", "mn", "Cyrl", "hyph-mn.tex", 2, 2),
+        ("Dutch", "nl", "Latn", "hyph-nl.tex", 2, 2),
+        ("Norwegian", "no", "Latn", "hyph-no.tex", 2, 2),
+        ("Portuguese", "pt", "Latn", "hyph-pt.tex", 2, 3),
+        ("Russian", "ru", "Cyrl", "hyph-ru.tex", 2, 2),
+        ("Serbian", "sr", "Cyrl", "hyph-sh-cyrl.tex", 2, 2),
+        ("Slovak", "sk", "Latn", "hyph-sk.tex", 2, 3),
+        ("Slovenian", "sl", "Latn", "hyph-sl.tex", 2, 2),
+        ("Albanian", "sq", "Latn", "hyph-sq.tex", 2, 2),
+        ("Swedish", "sv", "Latn", "hyph-sv.tex", 2, 2),
+        ("Turkmen", "tk", "Latn", "hyph-tk.tex", 2, 2),
+        ("Turkish", "tr", "Latn", "hyph-tr.tex", 2, 2),
+        ("Ukrainian", "uk", "Cyrl", "hyph-uk.tex", 2, 2),
+    ];
+
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    // Build the tries.
+    for (_, iso, _, filename, ..) in languages {
+        let path = Path::new("patterns").join(filename);
+        let tex = fs::read_to_string(&path)?;
+        let mut builder = TrieBuilder::new();
+        parse(&tex, |pat| builder.insert(pat));
+        builder.compress();
+        let trie = builder.encode();
+        fs::write(out.join(format!("{iso}.bin")), &trie)?;
+    }
+
+    let file = File::create(out.join("langs.rs"))?;
+    let mut w = BufWriter::new(file);
+
+    writeln!(
+        w,
+        "/// A language you can hyphenate in.
+        ///
+        /// Lists for each language also the ISO 639-1 two letter
+        /// language code and the ISO 15924 four letter script code.
+        #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+        #[non_exhaustive]
+        pub enum Lang {{"
+    )?;
+
+    for (name, iso, script, ..) in languages {
+        writeln!(w, "  /// Hyphenation for _{name}_ (`{iso}`, `{script}`).")?;
+        writeln!(w, "  {name},")?;
+    }
+
+    writeln!(w, "}}")?;
+
+    writeln!(w, "impl Lang {{")?;
+
+    // Implementation of `from_iso`.
+    writeln!(w, "  /// Select a language using its ISO 639-1 code.")?;
+    writeln!(w, "  pub fn from_iso(iso: [u8; 2]) -> Option<Self> {{")?;
+    writeln!(w, "    match &iso {{")?;
+    for (name, iso, ..) in languages {
+        writeln!(w, r#"      b"{iso}" => Some(Self::{name}),"#)?;
+    }
+    writeln!(w, "      _ => None,")?;
+    writeln!(w, "    }}")?;
+    writeln!(w, "  }}")?;
+
+    // Implementation of `root`.
+    writeln!(w, "  fn root(self) -> State<'static> {{")?;
+    writeln!(w, "    match self {{")?;
+    for (name, iso, ..) in languages {
+        write!(w, "      Self::{name} => State::root(include_bytes!(")?;
+        write!(w, r#"concat!(env!("OUT_DIR"), "/{iso}.bin")"#)?;
+        writeln!(w, ")),")?;
+    }
+    writeln!(w, "    }}")?;
+    writeln!(w, "  }}")?;
+    writeln!(w, "}}")?;
+
+    Ok(())
+}
+
+/// Parse a TeX pattern file, calling `f` with each pattern.
+pub fn parse<F>(tex: &str, mut f: F)
+where
+    F: FnMut(&str),
+{
+    let mut s = Scanner(tex);
+    while let Some(c) = s.eat() {
+        match c {
+            '%' => {
+                s.eat_while(|c| c != '\n');
+            }
+            '\\' if s.eat_if("patterns{") => loop {
+                let pat = s.eat_while(|c| c != '}' && c != '%' && !c.is_whitespace());
+                if !pat.is_empty() {
+                    f(pat);
+                }
+                match s.eat() {
+                    Some('}') => break,
+                    Some('%') => s.eat_while(|c| c != '\n'),
+                    _ => s.eat_while(char::is_whitespace),
+                };
+            },
+            _ => {}
+        }
+    }
+}
+
+struct Scanner<'a>(&'a str);
+
+impl<'a> Scanner<'a> {
+    fn eat(&mut self) -> Option<char> {
+        let mut chars = self.0.chars();
+        let c = chars.next();
+        self.0 = chars.as_str();
+        c
+    }
+
+    fn eat_if(&mut self, pat: &str) -> bool {
+        let matches = self.0.starts_with(pat);
+        if matches {
+            self.0 = &self.0[pat.len() ..];
+        }
+        matches
+    }
+
+    fn eat_while(&mut self, f: fn(char) -> bool) -> &'a str {
+        let mut offset = 0;
+        let mut chars = self.0.chars();
+        while chars.next().map_or(false, f) {
+            offset = self.0.len() - chars.as_str().len();
+        }
+        let head = &self.0[.. offset];
+        self.0 = &self.0[offset ..];
+        head
+    }
+}
 
 /// Builds a trie from patterns.
-pub struct TrieBuilder {
-    pub root: usize,
-    pub nodes: Vec<Node>,
-    pub levels: Vec<(usize, u8)>,
+struct TrieBuilder {
+    root: usize,
+    nodes: Vec<Node>,
+    levels: Vec<(usize, u8)>,
 }
 
 /// A node in the trie.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
-pub struct Node {
-    pub trans: Vec<u8>,
-    pub targets: Vec<usize>,
-    pub levels: Option<(usize, usize)>,
+struct Node {
+    trans: Vec<u8>,
+    targets: Vec<usize>,
+    levels: Option<(usize, usize)>,
 }
 
 impl TrieBuilder {
     /// Create a new trie with just the root node.
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             root: 0,
             nodes: vec![Node::default()],
@@ -26,7 +187,7 @@ impl TrieBuilder {
     }
 
     /// Insert a pattern like `.a1bc2d` into the trie.
-    pub fn insert(&mut self, pattern: &str) {
+    fn insert(&mut self, pattern: &str) {
         let mut state = 0;
         let mut dist = 0;
         let mut levels = vec![];
@@ -68,7 +229,7 @@ impl TrieBuilder {
     }
 
     /// Perform suffix compression on the trie.
-    pub fn compress(&mut self) {
+    fn compress(&mut self) {
         let mut map = HashMap::new();
         let mut new = vec![];
         self.root = self.compress_node(0, &mut map, &mut new);
@@ -94,7 +255,7 @@ impl TrieBuilder {
     }
 
     /// Encode the tree.
-    pub fn encode(&self) -> Vec<u8> {
+    fn encode(&self) -> Vec<u8> {
         let start = 4 + self.levels.len();
 
         // Compute an address estimate for each node. We can't know the final
@@ -158,7 +319,6 @@ impl TrieBuilder {
 
             if let Some((offset, len)) = node.levels {
                 let offset = 4 + offset;
-
                 assert!(offset < 4096, "too high level offset");
                 assert!(len < 16, "too high level count");
 
@@ -179,93 +339,6 @@ impl TrieBuilder {
         }
 
         data
-    }
-}
-
-/// A state in a trie traversal.
-#[derive(Copy, Clone)]
-pub struct State<'a> {
-    data: &'a [u8],
-    addr: usize,
-    stride: usize,
-    levels: &'a [u8],
-    trans: &'a [u8],
-    targets: &'a [u8],
-}
-
-impl<'a> State<'a> {
-    /// Create a new state at the root node.
-    pub fn root(data: &'a [u8]) -> Self {
-        let bytes = data[.. 4].try_into().unwrap();
-        let addr = u32::from_be_bytes(bytes) as usize;
-        Self::at(data, addr)
-    }
-
-    /// Create a new state at the given node address.
-    pub fn at(data: &'a [u8], addr: usize) -> Self {
-        let node = &data[addr ..];
-        let mut pos = 0;
-
-        // Decode whether the state has levels and the transition count.
-        let has_levels = node[pos] >> 7 != 0;
-        let stride = usize::from((node[pos] >> 5) & 3);
-        let mut count = usize::from(node[pos] & 31);
-        pos += 1;
-
-        // Possibly decode high transition count.
-        if count == 31 {
-            count = usize::from(node[pos]);
-            pos += 1;
-        }
-
-        // Decode the levels.
-        let mut levels: &[u8] = &[];
-        if has_levels {
-            let offset_hi = usize::from(node[pos]) << 4;
-            let offset_lo = usize::from(node[pos + 1]) >> 4;
-            let offset = offset_hi | offset_lo;
-            let len = usize::from(node[pos + 1] & 15);
-            levels = &data[offset .. offset + len];
-            pos += 2;
-        }
-
-        // Decode the transitions.
-        let trans = &node[pos .. pos + count];
-        pos += count;
-
-        // Decode the targets.
-        let targets = &node[pos .. pos + stride * count];
-
-        Self {
-            data,
-            addr,
-            stride,
-            levels,
-            trans,
-            targets,
-        }
-    }
-
-    /// Return the state reached by following the transition labelled `b`.
-    /// Returns `None` if there is no such state.
-    pub fn transition(self, b: u8) -> Option<Self> {
-        self.trans.iter().position(|&x| x == b).map(|idx| {
-            let offset = self.stride * idx;
-            let delta = from_be_bytes(&self.targets[offset .. offset + self.stride]);
-            let next = (self.addr as isize + delta) as usize;
-            Self::at(self.data, next)
-        })
-    }
-
-    /// Returns the levels contained in the state.
-    pub fn levels(self) -> impl Iterator<Item = (usize, u8)> + 'a {
-        let mut offset = 0;
-        self.levels.iter().map(move |&packed| {
-            let dist = usize::from(packed / 10);
-            let level = packed % 10;
-            offset += dist;
-            (offset, level)
-        })
     }
 }
 
@@ -293,23 +366,6 @@ fn to_be_bytes(buf: &mut Vec<u8>, num: isize, stride: usize) {
         buf.push((unsigned >> 16) as u8);
         buf.push((unsigned >> 8) as u8);
         buf.push(unsigned as u8);
-    } else {
-        panic!("invalid stride");
-    }
-}
-
-/// Decode a signed number with 1, 2 or 3 bytes.
-fn from_be_bytes(buf: &[u8]) -> isize {
-    if let Ok(array) = buf.try_into() {
-        i8::from_be_bytes(array) as isize
-    } else if let Ok(array) = buf.try_into() {
-        i16::from_be_bytes(array) as isize
-    } else if buf.len() == 3 {
-        let first = usize::from(buf[0]) << 16;
-        let second = usize::from(buf[1]) << 8;
-        let third = usize::from(buf[2]);
-        let unsigned = first | second | third;
-        unsigned as isize - (1 << 23)
     } else {
         panic!("invalid stride");
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-mod tex;
-mod trie;
+//! `hypher` separates words into syllables.
+//!
+//! # Features
+//! - All-inclusive: Hyphenation patterns are embedded into the binary as
+//!   efficiently encoded finite automata at build time.
+//! - Zero startup time: Hyphenation automata operate directly over the embedded
+//!   binary data with no up-front decoding.
+//! - No allocations unless when hyphenating very long words (> 40 bytes).
+//! - Support for many languages.
+//!
+//! # Example
+//! ```
+//! use hypher::{hyphenate, Lang};
+//!
+//! let syllables = hyphenate("extensive", Lang::English);
+//! let joined = syllables.collect::<Vec<_>>().join("-");
+//! assert_eq!(joined, "ex-ten-sive");
+//! ```
 
 /// Segment a word into syllables.
-pub fn hyphenate(word: &str) -> impl Iterator<Item = &str> {
-    let mut builder = trie::TrieBuilder::new();
-    let patterns = include_str!("../patterns/hyph-en-us.tex");
-    tex::parse(patterns, |pat| builder.insert(pat));
-    builder.compress();
-    let trie = builder.encode();
-
+pub fn hyphenate(word: &str, lang: Lang) -> impl Iterator<Item = &str> {
     // The level between each two inner bytes of the word.
     let len = word.len().saturating_sub(1);
     let mut levels = vec![0; len];
@@ -16,7 +26,7 @@ pub fn hyphenate(word: &str) -> impl Iterator<Item = &str> {
     // Start pattern matching at each character boundary.
     let dotted = format!(".{}.", word.to_ascii_lowercase());
     for (start, _) in dotted.char_indices() {
-        let mut state = trie::State::root(&trie);
+        let mut state = lang.root();
         for b in dotted[start ..].bytes() {
             if let Some(next) = state.transition(b) {
                 state = next;
@@ -49,67 +59,167 @@ pub fn hyphenate(word: &str) -> impl Iterator<Item = &str> {
         })
 }
 
+// Include language data.
+include!(concat!(env!("OUT_DIR"), "/langs.rs"));
+
+/// A state in a trie traversal.
+#[derive(Copy, Clone)]
+struct State<'a> {
+    data: &'a [u8],
+    addr: usize,
+    stride: usize,
+    levels: &'a [u8],
+    trans: &'a [u8],
+    targets: &'a [u8],
+}
+
+impl<'a> State<'a> {
+    /// Create a new state at the root node.
+    fn root(data: &'a [u8]) -> Self {
+        let bytes = data[.. 4].try_into().unwrap();
+        let addr = u32::from_be_bytes(bytes) as usize;
+        Self::at(data, addr)
+    }
+
+    /// Create a new state at the given node address.
+    fn at(data: &'a [u8], addr: usize) -> Self {
+        let node = &data[addr ..];
+        let mut pos = 0;
+
+        // Decode whether the state has levels and the transition count.
+        let has_levels = node[pos] >> 7 != 0;
+        let stride = usize::from((node[pos] >> 5) & 3);
+        let mut count = usize::from(node[pos] & 31);
+        pos += 1;
+
+        // Possibly decode high transition count.
+        if count == 31 {
+            count = usize::from(node[pos]);
+            pos += 1;
+        }
+
+        // Decode the levels.
+        let mut levels: &[u8] = &[];
+        if has_levels {
+            let offset_hi = usize::from(node[pos]) << 4;
+            let offset_lo = usize::from(node[pos + 1]) >> 4;
+            let offset = offset_hi | offset_lo;
+            let len = usize::from(node[pos + 1] & 15);
+            levels = &data[offset .. offset + len];
+            pos += 2;
+        }
+
+        // Decode the transitions.
+        let trans = &node[pos .. pos + count];
+        pos += count;
+
+        // Decode the targets.
+        let targets = &node[pos .. pos + stride * count];
+
+        Self {
+            data,
+            addr,
+            stride,
+            levels,
+            trans,
+            targets,
+        }
+    }
+
+    /// Return the state reached by following the transition labelled `b`.
+    /// Returns `None` if there is no such state.
+    fn transition(self, b: u8) -> Option<Self> {
+        self.trans.iter().position(|&x| x == b).map(|idx| {
+            let offset = self.stride * idx;
+            let delta = from_be_bytes(&self.targets[offset .. offset + self.stride]);
+            let next = (self.addr as isize + delta) as usize;
+            Self::at(self.data, next)
+        })
+    }
+
+    /// Returns the levels contained in the state.
+    fn levels(self) -> impl Iterator<Item = (usize, u8)> + 'a {
+        let mut offset = 0;
+        self.levels.iter().map(move |&packed| {
+            let dist = usize::from(packed / 10);
+            let level = packed % 10;
+            offset += dist;
+            (offset, level)
+        })
+    }
+}
+
+/// Decode a signed number with 1, 2 or 3 bytes.
+fn from_be_bytes(buf: &[u8]) -> isize {
+    if let Ok(array) = buf.try_into() {
+        i8::from_be_bytes(array) as isize
+    } else if let Ok(array) = buf.try_into() {
+        i16::from_be_bytes(array) as isize
+    } else if buf.len() == 3 {
+        let first = usize::from(buf[0]) << 16;
+        let second = usize::from(buf[1]) << 8;
+        let third = usize::from(buf[2]);
+        let unsigned = first | second | third;
+        unsigned as isize - (1 << 23)
+    } else {
+        panic!("invalid stride");
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{hyphenate, Lang};
+    use Lang::*;
 
-    fn test(hyphenated: &str) {
+    fn test(lang: Lang, hyphenated: &str) {
         let word = hyphenated.replace('-', "");
-        let parts = hyphenate(&word).collect::<Vec<_>>();
+        let parts = hyphenate(&word, lang).collect::<Vec<_>>();
         let joined = parts.join("-");
+        println!("{joined}");
         assert_eq!(joined, hyphenated);
     }
 
     #[test]
-    fn test_stats() {
-        let mut pat_sum = 0;
-        let mut trie_sum = 0;
-
-        for entry in std::fs::read_dir("patterns").unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-            println!("---------------------------");
-            println!("{}", path.display());
-
-            let mut builder = trie::TrieBuilder::new();
-            let tex = std::fs::read_to_string(&path).unwrap();
-            tex::parse(&tex, |pat| builder.insert(pat));
-            builder.compress();
-            let trie = builder.encode();
-
-            pat_sum += tex.len();
-            trie_sum += trie.len();
-
-            println!("  Pattern size: {} byte", tex.len());
-            println!("  Trie size:    {} byte", trie.len());
-            println!(
-                "  Percentage:   {:.0}%",
-                100.0 * (trie.len() as f64 / tex.len() as f64),
-            );
-            println!();
-        }
-
-        println!("===========================");
-        println!("Total pattern size: {pat_sum} byte");
-        println!("Total tries size:   {trie_sum} byte");
-        println!(
-            "Percentage:         {:.0}%",
-            100.0 * (trie_sum as f64 / pat_sum as f64),
-        );
-        println!();
+    fn test_english() {
+        test(English, "hi");
+        test(English, "hel-lo");
+        test(English, "wel-come");
+        test(English, "walk-ing");
+        test(English, "cap-tiVe");
+        test(English, "pur-sue");
+        test(English, "wHaT-eV-eR");
+        test(English, "bro-ken");
+        test(English, "ex-ten-sive");
+        test(English, "Prob-a-bil-i-ty");
+        test(English, "col-or");
+        test(English, "rec-og-nize");
     }
 
     #[test]
-    fn test_hyphenate() {
-        test("hi");
-        test("hel-lo");
-        test("wel-come");
-        test("walk-ing");
-        test("cap-tiVe");
-        test("pur-sue");
-        test("wHaT-eV-eR");
-        test("bro-ken");
-        test("ex-ten-sive");
-        test("Prob-a-bil-i-ty");
+    fn test_german() {
+        test(German, "Baum");
+        test(German, "ge-hen");
+        test(German, "Ap-fel");
+        test(German, "To-ma-te");
+        test(German, "Ein-ga-be-auf-for-de-rung");
+        test(German, "Fort-pflan-zungs-lem-ma");
+        test(German, "stra-te-gie-er-hal-ten-den");
+        test(German, "hübsch");
+        test(German, "häss-lich");
+        test(German, "über-zeu-gen-der");
+    }
+
+    #[test]
+    fn test_greek() {
+        test(Greek, "δια-με-ρί-σμα-τα");
+        test(Greek, "λα-τρευ-τός");
+        test(Greek, "κά-τοι-κος");
+    }
+
+    #[test]
+    fn test_georgian() {
+        test(Georgian, "თა-რო");
+        test(Georgian, "შეყ-ვა-ნა");
+        test(Georgian, "კარ-ტო-ფი-ლი");
     }
 }
